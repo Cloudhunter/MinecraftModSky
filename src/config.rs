b8112b9258e08123604f1b3f3b76239afa2bc29e
Aaron Mills <aaron_m@creeperhost.net>
2024-11-17 19:25:45 +0000

    Remove env variable

--- src/config.rs
+++ src/config.rs
@@ -4,7 +4,6 @@ use dotenv::dotenv;
 use std::env;
 
 pub struct Config {
-    pub chat_gpt_api_key: String,
     pub database_url: String,
     pub feed_generator_did: Did,
     pub publisher_did: Did,
@@ -16,7 +15,6 @@ impl Config {
         dotenv()?;
 
         Ok(Self {
-            chat_gpt_api_key: env::var("CHAT_GPT_API_KEY")?,
             database_url: env::var("DATABASE_URL")?,
             feed_generator_hostname: env::var("FEED_GENERATOR_HOSTNAME")?,
             feed_generator_did: format!("did:web:{}", env::var("FEED_GENERATOR_HOSTNAME")?)
